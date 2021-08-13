3efbe18156cc2b918f27379ce76fe91fdfffcb5b
MrRobo-t <85021988+MrRobo-t@users.noreply.github.com>
2021-08-13 10:50:48 +0530

    Assignment 4

--- wifi.py
+++ wifi.py
@@ -1,15 +1,22 @@
 import subprocess
 from wireless import Wireless
+import stdiomask
+from getpass import getpass
 
 
 class Wifi:
     def find_best_wifi(self):
-        best_wifi = "a"
+        '''
+        description: This function is used to find the available networks which is
+        stored in devices_data. The user gets to choose the wifi he/she wants to
+        connect with and provide the password for the same.
+        :return: not required
+        '''
 
-        # using the check_output() for having the network term retrival
+        # Fetching network term retrival
         devices_data = subprocess.check_output(['netsh', 'wlan', 'show', 'network'])
 
-        # decode it to strings
+        # Converting data to String
         devices_data = devices_data.decode('ascii')
         devices_data = devices_data.replace("\r", "")
 
@@ -28,11 +35,18 @@ class Wifi:
         for wifi in network:
             print("[" + str(i) + "] " + wifi)
             i = i + 1
+
+        print("Your choice?")
         select_wifi = input()
 
-        wire = Wireless()
-        wire.connect(ssid=network[int(select_wifi)-1], password='hitman123')
-        print(network[int(select_wifi)-1])
+        wire = Wireless('wlan1')
+        passwd = stdiomask.getpass(mask='*')
+        status = wire.connect(ssid=network[int(select_wifi)-1], password=passwd)
+        if status == True:
+            print("connected!")
+        else:
+            print("Incorrect password!")
+
 
 w = Wifi()
 w.find_best_wifi()
